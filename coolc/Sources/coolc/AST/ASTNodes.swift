99b5b66da2d5d17caea6ba12e783d87f9c81f3ff
2BJake <jakef@fastmail.com>
2020-08-12 20:58:34 -0500

    move stuff around

--- coolc/Sources/coolc/AST/ASTNodes.swift
+++ coolc/Sources/coolc/AST/ASTNodes.swift
@@ -7,57 +7,6 @@
 
 import Foundation
 
-enum ClassType {
-    case none
-    case selfType
-    case object
-    case io
-    case bool
-    case int
-    case string
-    case main
-    case defined(IdSymbol)
-}
-
-extension ClassType {
-    static let constantTypes = [ClassType.bool, .string, .int]
-    static let builtInTypes = [ClassType.object, .int, .bool, .string, .io]
-    var isBuiltInClass: Bool { Self.builtInTypes.contains(self) }
-}
-
-extension ClassType: CustomStringConvertible {
-    init(_ id: IdSymbol) {
-        switch id {
-            case .selfTypeName: self = .selfType
-            case .objectTypeName: self = .object
-            case .ioTypeName: self = .io
-            case .boolTypeName: self = .bool
-            case .intTypeName: self = .int
-            case .stringTypeName: self = .string
-            case .mainTypeName: self = .main
-            default:
-                precondition(id.value.first?.isUppercase == true)
-                self = .defined(id)
-        }
-    }
-
-    var description: String {
-        switch self {
-            case .none: return IdSymbol.noClass.value
-            case .selfType: return IdSymbol.selfTypeName.value
-            case .object: return IdSymbol.objectTypeName.value
-            case .io: return IdSymbol.ioTypeName.value
-            case .bool: return IdSymbol.boolTypeName.value
-            case .int: return IdSymbol.intTypeName.value
-            case .string: return IdSymbol.stringTypeName.value
-            case .main: return IdSymbol.mainTypeName.value
-            case .defined(let id): return id.value
-        }
-    }
-}
-
-extension ClassType: Hashable, Equatable {}
-
 struct SourceLocation {
     let fileName: String
     let lineNumber: Int
